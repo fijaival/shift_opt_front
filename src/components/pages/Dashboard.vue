818aea7984e72fab41de265aed52f58720207e00
fujiapp <8_carpboy_74@au.com>
2024-02-03 00:30:39 +0900

    ダッシュボード微修正

--- src/components/pages/Dashboard.vue
+++ src/components/pages/Dashboard.vue
@@ -6,24 +6,21 @@ export default {
 </script>
 
 <template>
-  <section class="text-gray-600 body-font">
+  <section class="text-gray-900 h-ful bg-red-50">
     <div class="container px-5 py-24 mx-auto flex flex-wrap">
       <div class="lg:w-2/3 mx-auto">
-        <router-link class="mr-5 hover:text-gray-900" to="/">
+        <router-link
+          to="/"
+          class="block hover:shadow-xl transition-shadow duration-300 ease-in-out"
+        >
           <div
-            class="flex flex-wrap w-full bg-gray-100 py-32 px-10 relative mb-4"
+            class="flex flex-wrap w-full bg-white py-32 px-10 relative mb-4 shadow-lg rounded-lg"
           >
-            <img
-              alt="gallery"
-              class="w-full object-cover h-full object-center block opacity-25 absolute inset-0"
-              src="../../../public/ffaaaa.png"
-            />
             <div class="text-center relative z-10 w-full">
-              <h2 class="text-4xl text-gray-900 font-medium title-font mb-2">
+              <h2 class="text-4xl text-red-600 font-semibold title-font mb-2">
                 シフト希望入力
               </h2>
-
-              <p class="leading-relaxed text-xl">
+              <p class="leading-relaxed text-lg">
                 シフトの希望を入力しましょう
               </p>
             </div>
@@ -31,21 +28,19 @@ export default {
         </router-link>
         <div class="flex flex-wrap -mx-2">
           <div class="px-2 w-1/2">
-            <router-link class="mr-5 hover:text-gray-900" to="/driver">
+            <router-link
+              to="/driver"
+              class="block hover:shadow-xl transition-shadow duration-300 ease-in-out"
+            >
               <div
-                class="flex flex-wrap w-full bg-gray-100 sm:py-24 py-16 sm:px-10 px-6 relative"
+                class="flex flex-wrap w-full bg-white sm:py-24 py-16 sm:px-10 px-6 relative shadow-lg rounded-lg"
               >
-                <img
-                  alt="gallery"
-                  class="w-full object-cover h-full object-center block opacity-25 absolute inset-0"
-                  src="../../../public/ffaaaa.png"
-                />
                 <div class="text-center relative z-10 w-full">
-                  <h2 class="text-xl text-gray-900 font-medium title-font mb-2">
+                  <h2 class="text-xl text-red-600 font-medium title-font mb-2">
                     自動生成
                   </h2>
                   <p class="leading-relaxed">シフト希望の入力が完了したら</p>
-                  <p class="leading-relaxed inline-flex">
+                  <p class="leading-relaxed">
                     AIにシフトを作ってもらいましょう！
                   </p>
                 </div>
@@ -54,23 +49,19 @@ export default {
           </div>
 
           <div class="px-2 w-1/2">
-            <router-link class="mr-5 hover:text-gray-900" to="/driver">
+            <router-link
+              to="/driver"
+              class="block hover:shadow-xl transition-shadow duration-300 ease-in-out"
+            >
               <div
-                class="flex flex-wrap w-full bg-gray-100 sm:py-24 py-16 sm:px-10 px-6 relative"
+                class="flex flex-wrap w-full bg-white sm:py-24 py-16 sm:px-10 px-6 relative shadow-lg rounded-lg"
               >
-                <img
-                  alt="gallery"
-                  class="w-full object-cover h-full object-center block opacity-25 absolute inset-0"
-                  src="../../../public/ffaaaa.png"
-                />
                 <div class="text-center relative z-10 w-full">
-                  <h2 class="text-xl text-gray-900 font-medium title-font mb-2">
+                  <h2 class="text-xl text-red-600 font-medium title-font mb-2">
                     従業員情報
                   </h2>
                   <p class="leading-relaxed">従業員情報・ドライバー情報の</p>
-                  <p class="leading-relaxed inline-flex">
-                    確認・登録・修正はこちら
-                  </p>
+                  <p class="leading-relaxed">確認・登録・修正はこちら</p>
                 </div>
               </div>
             </router-link>
@@ -79,5 +70,4 @@ export default {
       </div>
     </div>
   </section>
-  rinchan
 </template>
